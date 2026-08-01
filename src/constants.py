e1746352406cfae93a6c6443b6c5e46533e704b1
ywz knnd <akmn140@gmail.com>
2026-08-01 17:32:39 +0300

    Add some constants

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,7 @@
+import datetime 
+
 CONFIG_PATH = 'russian-telegram-news-analyzer/config.json'
+RAW_JSON_PATH = 'russian-telegram-news-analyzer/data/raw.json'
+
+OFFSET_DATE = datetime.date.today() - datetime.timedelta(days=2)
+CHANNELS: list[str]
