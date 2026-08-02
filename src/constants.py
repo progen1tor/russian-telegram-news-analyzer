ebd0494d9018455a1b7678fe272de8e8986d4b69
ywz knnd <akmn140@gmail.com>
2026-08-02 17:39:49 +0300

    Add tg channels & change offset date type

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,14 @@
 import datetime 
 
 CONFIG_PATH = 'russian-telegram-news-analyzer/config.json'
-RAW_JSON_PATH = 'russian-telegram-news-analyzer/data/raw.json'
 
-OFFSET_DATE = datetime.date.today() - datetime.timedelta(days=2)
-CHANNELS: list[str]
+OFFSET_DATE = datetime.datetime.today() - datetime.timedelta(days=90)
+CHANNELS: list[str] = [
+    'https://t.me/toporlive', 
+    'https://t.me/rt_russian', 
+    'https://t.me/rbc_news', 
+    'https://t.me/ria_novosti_russiya',
+    'https://t.me/rian_ru',
+    'https://t.me/bazabazon',
+    'https://t.me/novosti_russia360'
+    ]
