90578dc8cf7a7473b64c29a47ca5f40ac8ed158b
ywz knnd <akmn140@gmail.com>
2026-08-02 18:06:10 +0300

    1st version of collector

---
+++ src/tg_collector.py
@@ -0,0 +1,36 @@
+import asyncio 
+from telethon import TelegramClient
+from typing import Any 
+from constants import OFFSET_DATE, CHANNELS
+from utils import reaction_handler
+
+
+async def tg_collector(client: TelegramClient, channel_link: str) -> list[dict[str, Any]]: 
+    channel_records = []
+    
+    async for msg in client.iter_messages(channel_link, offset_date=OFFSET_DATE, reverse=True):
+        reactions_data = reaction_handler(msg.reactions)
+        
+        channel_records.append({
+            'channel': channel_link,
+            'message_id': msg.id, 
+            'datetime': msg.date, 
+            'date': msg.date.date(),  
+            'text': msg.message,
+            'text_length': len(msg.message),
+            'has_media': bool(msg.media),
+            'views_count': msg.views, 
+            'forwards_count': msg.forwards,
+            'reactions_count': reactions_data[0],
+            'most_used_reaction': reactions_data[1]
+        })
+        
+    return channel_records
+
+
+async def gatherer(client: TelegramClient) -> list[dict[str, Any]]:
+    coros = []
+    for channel in CHANNELS:
+        coros.append(tg_collector(client, channel))
+        
+    return await asyncio.gather(*coros)
