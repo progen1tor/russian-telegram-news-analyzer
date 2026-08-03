f8022d9f7a652326f448ee34de8974e039eb0fee
ywz knnd <akmn140@gmail.com>
2026-08-03 17:31:58 +0300

    Add custom emoji processing

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,9 @@ def reaction_handler(reaction_object: types.MessageReactions) -> tuple[int, str 
     if reaction_object:
         reactions_data = reaction_object.results
         ttl_reactions_count = sum(r.count for r in reactions_data)
-        most_used_reaction = reactions_data[0].reaction.emoticon
+        try: 
+            most_used_reaction = reactions_data[0].reaction.emoticon
+        except AttributeError:
+            most_used_reaction = 'CUSTOM_EMOJI'
         return ttl_reactions_count, most_used_reaction
     return 0, None 
