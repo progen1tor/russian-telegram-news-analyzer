786727c60e21a26c7955cab16578bcf4ce89bd85
ywz knnd <akmn140@gmail.com>
2026-08-03 17:38:59 +0300

    Add skipping msgs w/o text

--- src/tg_collector.py
+++ src/tg_collector.py
@@ -10,6 +10,9 @@ async def tg_collector(client: TelegramClient, channel_link: str) -> list[dict[s
     channel_records = []
     
     async for msg in client.iter_messages(channel_link, offset_date=OFFSET_DATE, reverse=True):
+        if not msg.message:
+            continue
+        
         reactions_data = reaction_handler(msg.reactions)
         
         channel_records.append({
@@ -25,7 +28,7 @@ async def tg_collector(client: TelegramClient, channel_link: str) -> list[dict[s
             'forwards_count': msg.forwards,
             'reactions_count': reactions_data[0],
             'most_used_reaction': reactions_data[1]
-        })  # ? collected_at ? 
+        })
         
     return channel_records
 
