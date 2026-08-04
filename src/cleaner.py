83cb0ed8aeab04608096a78decaf07ce865314a0
ywz knnd <akmn140@gmail.com>
2026-08-04 17:15:04 +0300

    Add preprocessing df funcs

---
+++ src/cleaner.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+
+def type_corrector(df: pd.DataFrame) -> pd.DataFrame:
+    df.collected_at = pd.to_datetime(df.collected_at, format='%Y-%m-%d %H:%M:%S.%f')  
+    df['datetime_utc'] = pd.to_datetime(df['datetime_utc'], format='%Y-%m-%d %H:%M:%S%z')
+    df['datetime_msc'] = pd.to_datetime(df['datetime_msc'], format='%Y-%m-%d %H:%M:%S%z')
+    df['date'] = pd.to_datetime(df['date'])
+    return df 
+
+
+def stripper(df: pd.DataFrame) -> pd.DataFrame:
+    df.text = df.text.str.strip()
+    return df 
+
+
+def sorter(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.sort_values(['channel', 'datetime_utc']).reset_index(drop=True)
+    return df 
