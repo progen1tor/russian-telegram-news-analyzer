a35248a91fdc172007b99a991b7823891d89adc1
ywz knnd <akmn140@gmail.com>
2026-08-06 18:02:45 +0300

    Sort functions & make some minor fixes

--- src/processor.py
+++ src/processor.py
@@ -1,6 +1,6 @@
 import pandas as pd 
-import string 
 
+# === BASE STATISTCIS ===
 
 def tg_channels_by_message_count(df: pd.DataFrame) -> pd.DataFrame:
     channels = df.groupby('channel').agg(message_count=('message_id', 'count'))
@@ -22,10 +22,18 @@ def most_active_time(df: pd.DataFrame) -> pd.DataFrame:
     return hours.sort_values('message_count', ascending=False)
 
 
+# === CHANNELS POPULARITY === 
+
 def tg_channels_by_views_count(df: pd.DataFrame) -> pd.DataFrame:
     channels = df.groupby('channel').agg(total_views=('views_count', 'sum'))
     return channels.sort_values('total_views', ascending=False)
 
+
+def tg_channels_by_subscribers(df: pd.DataFrame) -> pd.DataFrame:
+    ...
+
+
+# === MOST INTERESTING MESSAGES === 
 
 def most_popular_message(df: pd.DataFrame) -> pd.DataFrame:
     channels = df.groupby('channel', as_index=False)['views_count'].idxmax().set_index('views_count')
@@ -39,11 +47,13 @@ def most_reacted_message(df: pd.DataFrame) -> pd.DataFrame:
     return res.reset_index(drop=True).sort_values('reactions_count', ascending=False, ignore_index=True)
 
 
-def lonhgest_message(df: pd.DataFrame) -> pd.DataFrame:
+def longest_message(df: pd.DataFrame) -> pd.DataFrame:
     channels = df.groupby('channel', as_index=False)['text_length'].idxmax().set_index('text_length')
     res = channels.join(df, rsuffix='_source')[['channel', 'message_id', 'date', 'text', 'text_length']]
     return res.reset_index(drop=True).sort_values('text_length', ascending=False, ignore_index=True)
 
+
+# === AUDIENCE BEHAVIOR === 
 
 def most_used_reaction_by_channel(df: pd.DataFrame) -> pd.DataFrame:
     copied = df.copy()
