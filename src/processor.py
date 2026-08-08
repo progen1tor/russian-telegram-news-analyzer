63285e50f49239ad56c5e43787e58bd5880e99ed
ywz knnd <akmn140@gmail.com>
2026-08-08 18:08:10 +0300

    Add new func & make minor fixes

--- src/processor.py
+++ src/processor.py
@@ -1,9 +1,10 @@
-import pandas as pd 
+import pandas as pd
+from .utils import best_message_by
 
 # === BASE STATISTCIS ===
 
 def tg_channels_by_message_count(df: pd.DataFrame) -> pd.DataFrame:
-    channels = df.groupby('channel').agg(message_count=('message_id', 'count'))
+    channels = df.groupby(['channel', 'channel_title']).agg(message_count=('message_id', 'count'))
     return channels.sort_values('message_count', ascending=False)
 
 
@@ -25,32 +26,24 @@ def most_active_time(df: pd.DataFrame) -> pd.DataFrame:
 # === CHANNELS POPULARITY === 
 
 def tg_channels_by_views_count(df: pd.DataFrame) -> pd.DataFrame:
-    channels = df.groupby('channel').agg(total_views=('views_count', 'sum'))
+    channels = df.groupby(['channel', 'channel_title']).agg(total_views=('views_count', 'sum'))
     return channels.sort_values('total_views', ascending=False)
 
 
 def tg_channels_by_subscribers(df: pd.DataFrame) -> pd.DataFrame:
-    ...
+    return df.groupby(['channel', 'channel_title']).subscribers_count.max().sort_values(ascending=False).to_frame()
 
 
 # === MOST INTERESTING MESSAGES === 
 
 def most_popular_message(df: pd.DataFrame) -> pd.DataFrame:
-    channels = df.groupby('channel', as_index=False)['views_count'].idxmax().set_index('views_count')
-    res = channels.join(df, rsuffix='_source')[['channel', 'message_id', 'date', 'text', 'views_count']]
-    return res.reset_index(drop=True).sort_values('views_count', ascending=False, ignore_index=True)
-
+    return best_message_by(df, 'views_count').head(20)  # выводить лучше первые 20
 
 def most_reacted_message(df: pd.DataFrame) -> pd.DataFrame:
-    channels = df.groupby('channel', as_index=False)['reactions_count'].idxmax().set_index('reactions_count')
-    res = channels.join(df, rsuffix='_source')[['channel', 'message_id', 'date', 'text', 'reactions_count']]
-    return res.reset_index(drop=True).sort_values('reactions_count', ascending=False, ignore_index=True)
+    return best_message_by(df, 'reactions_count').head(20)
 
-
-def longest_message(df: pd.DataFrame) -> pd.DataFrame:
-    channels = df.groupby('channel', as_index=False)['text_length'].idxmax().set_index('text_length')
-    res = channels.join(df, rsuffix='_source')[['channel', 'message_id', 'date', 'text', 'text_length']]
-    return res.reset_index(drop=True).sort_values('text_length', ascending=False, ignore_index=True)
+def lonhgest_message(df: pd.DataFrame) -> pd.DataFrame:
+    return best_message_by(df, 'text_length').head(20)
 
 
 # === AUDIENCE BEHAVIOR === 
@@ -59,7 +52,7 @@ def most_used_reaction_by_channel(df: pd.DataFrame) -> pd.DataFrame:
     copied = df.copy()
     copied = copied.loc[copied.most_used_reaction != 'CUSTOM_EMOJI']
     
-    grouped = copied.groupby(['channel']).most_used_reaction.value_counts().reset_index()
+    grouped = copied.groupby(['channel', 'channel_title']).most_used_reaction.value_counts().reset_index()
     grouped['rnk'] = grouped.groupby('channel')['count'].rank(method='dense', ascending=False).astype(int)
     
     return grouped.loc[grouped.rnk == 1].drop(columns='rnk').sort_values('count', ascending=False).rename(columns={'count': 'usage'})
