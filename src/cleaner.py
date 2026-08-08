0baafd510c46950b2f22c4a183062b3e160f7e1b
ywz knnd <akmn140@gmail.com>
2026-08-08 17:56:03 +0300

    Fix datetime format

--- src/cleaner.py
+++ src/cleaner.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def type_corrector(df: pd.DataFrame) -> pd.DataFrame:
-    df.collected_at = pd.to_datetime(df.collected_at, format='%Y-%m-%d %H:%M:%S.%f')  
+    df.collected_at = pd.to_datetime(df.collected_at, format='%Y-%m-%d %H:%M:%S.%f%z')  
     df['datetime_utc'] = pd.to_datetime(df['datetime_utc'], format='%Y-%m-%d %H:%M:%S%z')
     df['datetime_msc'] = pd.to_datetime(df['datetime_msc'], format='%Y-%m-%d %H:%M:%S%z')
     df['date'] = pd.to_datetime(df['date'])
